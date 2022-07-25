99a08efadcc31571bd1083a0456a9849fb8ad8c7
jokecloud <medalhaemerson@gmail.com>
2022-07-25 15:25:25 -0300

    a lot

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from recipes.views import home, recipes
+
+from . import views
+
+# recipes:recipe
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', home),
-    path('recipes/', recipes),
+    path('', views.home, name="home"),
+    path('recipes/category/<int:category_id>/',
+         views.category, name="category"),
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,10 +1,29 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from utils.recipes.factory import make_recipe
+from recipes.models import Recipe
 
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    recipes = Recipe.objects.filter(
+        is_published=True,
+    ).order_by('-id')
+    return render(request, 'recipes/pages/home.html', context={
+        'recipes': recipes,
+    })
 
 
-def recipes(request):
-    return render(request, 'recipes/pages/recipes.html')
+def category(request, category_id):
+    recipes = Recipe.objects.filter(
+        category__id=category_id,
+        is_published=True,
+    ).order_by('-id')
+    return render(request, 'recipes/pages/category.html', context={
+        'recipes': recipes,
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
+        'recipe': make_recipe(),
+        'is_detail_page': True,
+    })
